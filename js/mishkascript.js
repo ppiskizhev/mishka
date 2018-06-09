36ce8b33147506b2d26503fdded5eb963cc755d9
Peter Piskizhev <p.piskizhev@gmail.com>
2018-06-09 18:16:15 +0300

    added open/close menu script

--- js/mishkascript.js
+++ js/mishkascript.js
@@ -1,7 +1,10 @@
+// opening and closing the menu
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 navMain.classList.remove('main-nav--nojs');
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', openMenu);
+
+function openMenu() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
@@ -9,4 +12,5 @@ navToggle.addEventListener('click', function() {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
   }
-});
+}
+
